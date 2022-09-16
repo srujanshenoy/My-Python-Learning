8e5cbc6e4be19e03ad1cce50968e56c2d184570d
srujanshenoy <srujanshenoy@gmail.com>
2022-09-16 20:30:30 +0530

    made program function-based. made few bug fixes:-     1. multiplication function fixed     2. division function fixed new features:-     exponent function now supported

--- excel and python calculator/app.py
+++ excel and python calculator/app.py
@@ -1,10 +1,7 @@
 import openpyxl as xl
-from openpyxl.chart import Reference
 
 # def outcell_to_sum(cell,incell,outcell):
 #     pass
-
-
 
 
 workbook = xl.load_workbook("excelsheet.xlsx")
@@ -12,7 +9,8 @@ sheet = workbook["Sheet1"]
 
 operator_cell = sheet.cell(2, 2)
 operator_cell = operator_cell.value
-
+#definitions
+#cells
 # define in_1
 in_1 = sheet.cell(2, 1)
 in_1 = in_1.value
@@ -28,19 +26,81 @@ in_2 = float(in_2)
 # end
 
 outcell = sheet.cell(2, 5)
-if operator_cell == "+":
+#end
+
+
+# functions
+def add():
     sum_of_ins = in_1 + in_2
     outcell.value = sum_of_ins
-elif operator_cell == "-":
+
+def subtract():
     difference_of_ins = in_1 - in_2
     outcell.value = difference_of_ins
-elif operator_cell.lower == "x":
+
+def multiply():
     product_of_ins = in_1 * in_2
     outcell.value = product_of_ins
-elif operator_cell == "/":
+
+def divide():
     if in_2 == 0:
         outcell.value = "Error: cannot divide by zero"
-    quotient_of_ins = in_1 / in_2
+    else:
+        quotient_of_ins = in_1 / in_2
+        outcell.value = quotient_of_ins
+
+def exponentiate():
+    power_of_ins = in_1 ** in_2
+    outcell.value = power_of_ins
+
+#end
+
+#end
+
+
+
+# if operator_cell == "+":
+#     sum_of_ins = in_1 + in_2
+#     outcell.value = sum_of_ins
+
+
+if operator_cell == "+":
+    add()
+
+# elif operator_cell == "-":
+#     difference_of_ins = in_1 - in_2
+#     outcell.value = difference_of_ins
+
+elif operator_cell == "-":
+    subtract()
+
+# elif operator_cell == "x":
+#     product_of_ins = in_1 * in_2
+#     outcell.value = product_of_ins
+
+elif operator_cell == "x":
+    multiply()
+
+# elif operator_cell == "/":
+#     if in_2 == 0:
+#         outcell.value = "Error: cannot divide by zero"
+#     else:
+#         quotient_of_ins = in_1 / in_2
+#         outcell.value = quotient_of_ins
+
+elif operator_cell == "/":
+    divide()
+
+# elif operator_cell == "^":
+#     power_of_ins = in_1 ** in_2
+#     outcell.value = power_of_ins
+
+elif operator_cell == "^":
+    exponentiate()
+else:
+    outcell.value = "Error: operator not supported."
+
+
 # # elif in_1.__class__ == "str":
 #     outcell.value = "kindly enter a different value for input 1"
 # elif in_2.__class__ == "str":
@@ -49,4 +109,5 @@ elif operator_cell == "/":
 #     outcell.value = "kindly enter a different value for input 1 and 2"
 
 workbook.save("excelsheet.xlsx")
+# print(outcell.value)#
 
