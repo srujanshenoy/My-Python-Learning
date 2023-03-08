d6d9f675946a5fecdf945e278e37b4759841443c
srujanshenoy <srujanshenoy@gmail.com>
2023-03-08 17:39:27 +0530

    Update main.py

--- learning 6 - Learning Object Oriented Programming/oop 1/main.py
+++ learning 6 - Learning Object Oriented Programming/oop 1/main.py
@@ -1,5 +1,7 @@
 from utils import Math
 import utils
+
+
 while True:
     selection = utils.select()
     if selection == 1:  # ascending order
@@ -31,7 +33,7 @@ while True:
             if type(appending_num) == float:
                 unsorted_list.append(appending_num)
         print(Math.descending(Math(unsorted_list)))
-    
+
     # IMPORTANT NOTE
     # FOR CASE 1 & 2:
     # THERE IS A FUNCTION IN UTILS TO DO THIS, BUT IT IS FOR OTHER FUNCTIONS.
@@ -48,7 +50,35 @@ while True:
         print(a.Math.Geometry.Square.perimeter())
 
     elif selection == 5:  # rectangle area
-        l = utils.check_float("l")
-        b = utils.check_float("b")
+        l = utils.check_float("length")
+        b = utils.check_float("breadth")
         print(Math.Geometry.Rectangle.area(Math.Geometry.Rectangle(l, b)))
-        
+
+    elif selection == 6:  # rectangle perimeter
+        l = utils.check_float("length")
+        b = utils.check_float("breadth")
+        print(Math.Geometry.Rectangle.perimeter(Math.Geometry.Rectangle(l, b)))
+
+    elif selection == 7: #  triangle area
+        h = utils.check_float("height")
+        b = utils.check_float("breadth")
+        print(Math.Geometry.Triangle.area(Math.Geometry.Triangle(h, b, 0, 0, 0)))
+
+    elif selection == 8:  # triangle perimeter
+        S1 = utils.check_float("Side 1")
+        S2 = utils.check_float("Side 2")
+        S3 = utils.check_float("Side 3")
+        print(Math.Geometry.Triangle.perimeter(Math.Geometry.Triangle(0, 0, S1, S2, S3)))
+
+    elif selection == 9: # circle area
+        r = utils.check_float("Radius")
+        print(Math.Geometry.Circle.area(Math.Geometry.Circle(r)))
+
+    elif selection == 10: # circle perimeter(Circumference)
+        r = utils.check_float("Radius")
+        print(Math.Geometry.Circle.perimeter(Math.Geometry.Circle(r)))
+
+    else:
+        print("We dont know what happened, please close the console or terminal and restart the app.")
+        print("If you had typed 11, We will stop the program for you.")
+        break
