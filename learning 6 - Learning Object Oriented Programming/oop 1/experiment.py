2fb07420cd515e072a3c5788000f79a1a85a6a59
srujanshenoy <srujanshenoy@gmail.com>
2023-03-08 07:59:07 +0530

    Update experiment.py

--- learning 6 - Learning Object Oriented Programming/oop 1/experiment.py
+++ learning 6 - Learning Object Oriented Programming/oop 1/experiment.py
@@ -1,3 +1,9 @@
-from utils import Math
+from utils import *
 mylist = Math([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 print(mylist.ascending())
+
+circle = Math.Geometry.Circle(1)
+print(circle.area())
+
+Triangle = Math.Geometry.Triangle(12, 6, 12, 12, 12)
+print(Triangle.area())
