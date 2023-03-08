c8cee8371104ea135aa9fb9d430eeccacb08d224
srujanshenoy <srujanshenoy@gmail.com>
2023-03-08 07:57:39 +0530

    Update utils.py

--- learning 6 - Learning Object Oriented Programming/oop 1/utils.py
+++ learning 6 - Learning Object Oriented Programming/oop 1/utils.py
@@ -22,6 +22,102 @@ class Math:
 
             sequence[j + 1] = key
             # this inserts the value the same position as we need as
-            # j is defined as  i - 1 and we are doing j + 1
-            # the +1 & -1 cancel eachother so we end up inserting at the exact index
+            # j is defined as  i - 1, and we are doing j + 1
+            # the +1 & -1 cancel each other, so we end up inserting at the exact index
         return sequence
+
+    def descending(self):
+        sequence = self.numbers
+
+        for i in range(1, len(sequence)):
+            key = sequence[i]  # current value
+            j = i - 1  # previous index
+
+            while j >= 0 and sequence[j] < key:
+                # when j is a positive number, and the previous value is less than the current value:
+
+                sequence[j + 1] = sequence[j]
+                #        ^^^^^         ^^^^
+                #      current index   previous index
+
+                # So this means we are saying that the current index is now the previous index
+                j -= 1
+                # we have to decrement j to continue searching through the sequence.
+
+            sequence[j + 1] = key
+            # this inserts the value the same position as we need as
+            # j is defined as  i - 1, and we are doing j + 1
+            # the +1 & -1 cancel each other, so we end up inserting at the exact index
+        return sequence
+
+    # class Area:
+    #     def __init__(self, length:float, breadth:float, height:float, radius:float, side:float):
+    #         self.length = length
+    #         self.breadth = breadth
+    #         self.height = height
+    #         self.radius = radius
+    #         self.side = side
+    #
+    #     def rectangle(self):
+    #         l = self.length
+    #         b = self.breadth
+    #         return l*b
+    #
+    #     def square(self):
+    #         a = self.side
+    #         return a**2
+    #
+    #     def triangle(self):
+    #         h = self.height
+    #         b = self.breadth
+    #         return 1/2 * h * b
+
+    class Geometry:
+        class Square:
+            def __init__(self, a: float):
+                self.a = a
+
+            def area(self):
+                return self.a ** 2
+
+            def perimeter(self):
+                return self.a * 4
+
+        class Rectangle:
+            def __init__(self, length: float, breadth: float):
+                self.length = length
+                self.breadth = breadth
+
+            def area(self):
+                return self.length * self.breadth
+
+            def perimeter(self):
+                return 2 * (self.length + self.breadth)
+
+        class Circle:
+            def __init__(self, radius: float):
+                self.radius = radius
+
+            def area(self):
+                return 3.141592659 * self.radius ** 2
+
+            def perimeter(self):
+                return 2 * 3.141592659 * self.radius
+
+        class Triangle:
+            def __init__(self, height:float, breadth:float, S1:float, S2:float, S3:float):
+                self.height = height
+                self.breadth = breadth
+                self.S1 = S1
+                self.S2 = S2
+                self.S3 = S3
+
+
+            def area(self):
+                return 1/2 * self.height * self.breadth
+
+            def perimeter(self):
+                return self.S1 + self.S2 + self.S3
+
+
+
